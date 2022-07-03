921a8c98bfd79c60c8578b033dde0af800004797
nuruslaily <nuruslaily88@gmail.com>
2022-07-03 13:49:52 +0700

    color update

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const primaryColor = Colors.blue;
-const secondaryColor = Color.fromARGB(255, 255, 166, 0);
+const secondaryColor = Colors.blue;
 const backgroundColor = Color(0xFFF9F7FE);
 const textColor = Color.fromARGB(255, 75, 75, 75);
 const textColor2 = Colors.grey;
